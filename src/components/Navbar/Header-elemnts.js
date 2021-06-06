cd0fac118723352f7ba60d01c77f22be2644e277
MJ <mohamed.aref32@gmail.com>
2021-06-06 03:20:11 -0500

    restyling the header

---
+++ src/components/Navbar/Header-elemnts.js
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+import { GrCoatCheck } from "react-icons/gr";
+import { Link } from "react-router-dom";
+ 
+export const Nav = styled.nav`
+  background: white;
+  height: 220px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`;
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 50px;
+  padding-left: 50px;
+
+  @media screen and(max-width:991px) {
+    padding-right: 30px;
+    padding-left: 30px;
+  }
+`;
+
+export const NavLogo = styled(Link)`
+  color: #494645;
+  display: flex;
+  justify-content: center;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 12rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const NavIcon = styled(GrCoatCheck)`
+  margin-right: 0.5rem;
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  @media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+
+    opacity: 1;
+    transition: all 0.5s ease;
+    background: #101522;
+  }
+`;
+
+export const NavItem = styled.li`
+  height: 80px;
+  border-bottom: 2px solid transparent;
+  &:hover {
+    border-bottom: 2px solid #4b59f7;
+  }
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    &:hover {
+      border: none;
+    }
+  }
+`;
+
+export const NavItemBtn = styled.li`
+  @media screen and (max-width: 960px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 120px;
+  }
+`;
+
+export const NavLink = styled(Link)`
+  color: #494645;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  height: 100%;
+  @media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+    &:hover {
+      color: #4b59f7;
+      transition: all 0.3s ease;
+    }
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  padding: 8px 16px;
+  height: 100%;
+  width: 100%;
+  border: none;
+  outline: none;
+`;
